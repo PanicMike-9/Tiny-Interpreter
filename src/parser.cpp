08b0781b40c507bb3d32bd960439e93ce2fce5c1
PanicMike-9 <omsaudager990@gmail.com>
2026-04-17 17:51:39 +0000

    comment incorrect code

---
+++ src/parser.cpp
@@ -0,0 +1,25 @@
+#include "parser.hpp"
+
+void parser(const std::vector<Token> &tokens)
+{
+    int i = 0;
+    Token current_token = tokens[i];
+
+    while(i < tokens.size())
+    {
+    }
+//    for(auto t : tokens)
+//    {
+//        if(t.token == TokenType::IDENT) i++;
+//
+//        if(t.token == TokenType::ASSIGN) i++;
+//
+//        if(t.token == TokenType::NUMBER) i++;
+//
+//        if(t.token == TokenType::PLUS) i++;
+//
+//        if(t.token == TokenType::NUMBER) i++;
+//    }
+}
+
+
