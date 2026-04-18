b77f91484d450d685d6ef6fd5e01f6ce5497d649
PanicMike-9 <omsaudager990@gmail.com>
2026-04-18 15:39:39 +0000

    fix: formatting

--- src/parser.cpp
+++ src/parser.cpp
@@ -9,18 +9,10 @@ void parser(const std::vector<Token> &tokens)
     {
         Token current_token = tokens[i];
 
-        if(current_token.token == TokenType::IDENT)
-        {
-        }
-        else if(current_token.token == TokenType::ASSIGN)
-        {
-        }
-        else if(current_token.token == TokenType::NUMBER)
-        {
-        }
-        else if(current_token.token == TokenType::PLUS)
-        {
-        }
+        if(current_token.token == TokenType::IDENT) {}
+        else if(current_token.token == TokenType::ASSIGN) {}
+        else if(current_token.token == TokenType::NUMBER) {}
+        else if(current_token.token == TokenType::PLUS) {}
 
         std::cout << " " << current_token.value << " ";
 
