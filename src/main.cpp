d61991fb44665dbc8338a24d71e864361a1b2916
PanicMike-9 <omsaudager990@gmail.com>
2026-04-18 16:25:05 +0000

    refactor: add string to int conversion

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 int main()
 {
-    std::vector<Token> input_tokens{ {TokenType::IDENT, "val"}, {TokenType::ASSIGN, "="}, {TokenType::NUMBER, "10"}, {TokenType::PLUS, "+"}, {TokenType::NUMBER, "20"} };
+    //std::vector<Token> input_tokens{ {TokenType::IDENT, "val"}, {TokenType::ASSIGN, "="}, {TokenType::NUMBER, "10"}, {TokenType::PLUS, "+"}, {TokenType::NUMBER, "20"} };
+    std::vector<Token> input_tokens{ {TokenType::IDENT, "val"}, {TokenType::ASSIGN, "="}, {TokenType::NUMBER, "5"} };
 
     parser(input_tokens);
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,23 +1,41 @@
 #include "parser.hpp"
 #include <iostream>
+#include <string>
 
 void parser(const std::vector<Token> &tokens)
 {
     int i = 0;
 
+    std::string variable;
+    std::string num_str;
+
+    int number = 0;
+
     while(i < tokens.size())
     {
         Token current_token = tokens[i];
 
-        if(current_token.token == TokenType::IDENT) {}
+        if(current_token.token == TokenType::IDENT) 
+        {
+            variable = current_token.value;
+        }
+
         else if(current_token.token == TokenType::ASSIGN) {}
-        else if(current_token.token == TokenType::NUMBER) {}
+
+        else if(current_token.token == TokenType::NUMBER) 
+        {
+            num_str = current_token.value;
+            number = std::stoi(num_str);
+        }
+
         else if(current_token.token == TokenType::PLUS) {}
 
-        std::cout << " " << current_token.value << " ";
+        //std::cout << " " << current_token.value << " ";
 
         i++;
     }
+
+    std::cout << variable << '=' << number;
 
     std::cout << '\n';
 }
