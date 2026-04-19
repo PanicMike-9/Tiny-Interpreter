75ed92141bbbc264e9cbeb9c0d3bee1a26f0541c
PanicMike-9 <omsaudager990@gmail.com>
2026-04-19 19:09:09 +0000

    fix: plus operator not working

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,14 @@
 int main()
 {
     //std::vector<Token> input_tokens{ {TokenType::IDENT, "val"}, {TokenType::ASSIGN, "="}, {TokenType::NUMBER, "10"}, {TokenType::PLUS, "+"}, {TokenType::NUMBER, "20"} };
-    std::vector<Token> input_tokens{ {TokenType::IDENT, "val"}, {TokenType::ASSIGN, "="}, 
-                                     {TokenType::NUMBER, "3"}, {TokenType::PLUS, "+"}, {TokenType::NUMBER, "7"} };
+    std::vector<Token> input_tokens{ {TokenType::IDENT, "val"}, 
+                                     {TokenType::ASSIGN, "="}, 
+                                     {TokenType::NUMBER, "3"}, 
+                                     {TokenType::PLUS, "+"}, 
+                                     {TokenType::NUMBER, "7"},
+                                     {TokenType::PLUS, "+"}, 
+                                     {TokenType::NUMBER, "10"} 
+                                   };
 
     parser(input_tokens);
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -10,9 +10,10 @@ void parser(const std::vector<Token> &tokens)
     std::string variable;
     std::string num_str;
 
-    int number = 0;
     std::optional<int> left;
     std::optional<int> right;
+
+    TokenType current_op;
 
     while(i < tokens.size())
     {
@@ -23,30 +24,32 @@ void parser(const std::vector<Token> &tokens)
             variable = current_token.value;
         }
 
-        else if(current_token.token == TokenType::ASSIGN) {}
-
-        else if(current_token.token == TokenType::NUMBER) 
+        else if(current_token.token == TokenType::NUMBER)
         {
             num_str = current_token.value;
-            number = std::stoi(num_str);
 
-            if(!left) left = number;
-            else right = number;
+            if(!left) left = std::stoi(num_str);
+            else right = std::stoi(num_str);
         }
 
         else if(current_token.token == TokenType::PLUS)
         {
-            *left += *right;
+            current_op = TokenType::PLUS;
         }
 
-        //std::cout << " " << current_token.value << " ";
+        if(left && right && current_op == TokenType::PLUS)
+        {
+            *left += *right;
+            right = std::nullopt; // reset right for next values
+        }
 
+        else if(current_token.token == TokenType::ASSIGN) {}
+
+        //std::cout << " " << current_token.value << " ";
         i++;
     }
 
-    std::cout << *left << ' ' << *right << '\n';
     std::cout << variable << '=' << *left;
-
     std::cout << '\n';
 }
 
