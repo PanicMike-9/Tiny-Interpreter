f040b5617a41991bd77de890d437c4b660a1be82
PanicMike-9 <omsaudager990@gmail.com>
2026-05-04 17:02:43 +0000

    update: add debugging values for both tokens, test the new unary operator operation to confirm precise output

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,26 @@
 
 int main()
 {
-    std::string input1 = "val1 = 5 + 10 * 2 / 5";
+    std::string input1 = "val1 = 6 + 10 * -2 / 5";
     std::vector<Token> token1 = tokenize(input1);
     
     // debugging to check correct token values and correct tokenization
     for(const Token& i : token1)
     {
         std::cout << i.value << ' ';
-        std::cout << "type: " << static_cast<int>(i.token) << '\n';
+        std::cout << "token1 type: " << static_cast<int>(i.token) << '\n';
     }
+
 
     std::string input2 = "val2 = 3 + 5 * 3 / 2";
     std::vector<Token> token2 = tokenize(input2);
 
+    for(const Token& i : token2)
+    {
+        std::cout << i.value << ' ';
+        std::cout << "token2 type: " << static_cast<int>(i.token) << '\n';
+    }
+
     parse(token1);
-    parse(token2); // the answer is actually 10.5, but we get 10, because it's integer division, the decimal value gets truncated
+    parse(token2); // 10
 }
