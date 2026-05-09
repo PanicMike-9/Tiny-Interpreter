81f628aa5582d3d937ad2e9e1f1d76ae2d0fb29e
PanicMike-9 <omsaudager990@gmail.com>
2026-05-09 18:35:56 +0000

    update: add variable map and comment out the incorrect logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <string>
 #include <iostream>
-#include <unordered_map>
 
 int main()
 {
@@ -27,20 +26,8 @@ int main()
         std::cout << "token2 type: " << static_cast<int>(i.token) << '\n';
     }
 
+
     parse(token1); // 40.8333
     parse(token2); // 10.5
-
-    std::cout << '\n';
-
-    std::unordered_map<std::string, int> student_info;
-
-    student_info.insert( {"CowMan", 3} );
-    student_info.insert( {"MonkeyBoy", 2} );
-    student_info.insert( {"CatDog", 1} );
-
-    for(auto i : student_info)
-    {
-        std::cout << i.first << ": " << i.second << '\n';
-    }
 }
 
