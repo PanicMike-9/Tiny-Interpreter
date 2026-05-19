75fb81c8978fd19a6c77e4200d7cefb864b4d5a5
PanicMike-9 <omsaudager990@gmail.com>
2026-05-19 17:39:23 +0000

    update: add new input and token to test >= and <= comparison

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
         std::cout << i.value << " type: " << static_cast<int>(i.token) << '\n';
     }
 
-    std::string input4 = "a= 3 * 5 > 5 * 5";
+    std::string input4 = "a = 3 * 5 < 5 * 5";
     std::vector<Token> token4 = tokenize(input4);
 
     std::cout << "--- Token 4 ---\n";
@@ -43,9 +43,19 @@ int main()
         std::cout << i.value << " type: " << static_cast<int>(i.token) << '\n';
     }
 
+    std::string input5 = "b = 4 * 4 <= 4 * 4";
+    std::vector<Token> token5 = tokenize(input5);
+
+    std::cout << "--- Token 5 ---\n";
+    for(const Token& i : token5)
+    {
+        std::cout << i.value << " type: " << static_cast<int>(i.token) << '\n';
+    }
+
     parse(token1); // 50.8333
     parse(token2); // 610
     parse(token3); // 305.255
     parse(token4); // false
+    parse(token5); // true
 }
 
